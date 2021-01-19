e000545f0de8a50b1d81656727848f95717313e6
Kyrilios <Cyriljohnamoronio@outlook.com>
2021-01-19 13:30:56 +0800

    added project files

---
+++ Summing_Forces/Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour
+{
+    public Vector3 velocityVector;
+    public Vector3 netForceVector;
+    public List<Vector3> forceVectorList = new List<Vector3>();
+
+    void Start()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        Addforces();
+        if (netForceVector == Vector3.zero) {
+            transform.position += velocityVector * Time.deltaTime;
+        } else {
+            Debug.LogError("Unbalanced Force Detected.");
+        }
+    }
+
+    void Addforces() {
+        netForceVector =Vector3.zero;
+
+        foreach (Vector3 forceVector in forceVectorList){
+            netForceVector += forceVector;
+        }    
+    }
+}
